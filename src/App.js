ca5560e32e59af82ef3b896602d3e9b01142e83e
Sergio Perez <perezsergio97.sp@gmail.com>
2020-03-15 22:31:26 -0500

    everything set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
-import React from 'react';
-import './App.css';
-import Table from "./components/Table";
+
+// import React, {useState, useEffect} from "react";
+// import Main from "./pages/Index";
+// import SearchBar from "./components/search";
+// import api from "./utils/API";
+
+import React from "react";
+import Header from "./components/header"
+import Table from "./components/table";
+import Footer from "./components/footer"
+
+import "./App.css";
 
 function App() {
-  return <Table />;
+  return (
+    <div className="App">
+      <Header />
+      <Table />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
+
+// function App() {
+//   const[users, setUsers]=useState([]);
+//   const[search, setSearch]=useState(``);
+
+//   const handleInputChange = e => {
+//     setSearch(e.target.value);
+//     console.log(search)
+//   };
+
+//   useEffect(() => {
+//     api.allUsers(15).then(res => {
+//       setUsers(res.data.results);
+//     }).catch(err => console.log(err))
+//   },[]);
+
+//   return (
+//     <div className="App">
+//       <Main/>
+//       <SearchBar users={users} search={search}/>
+//       <Table users={users} search={search} handleInputChange={handleInputChange}/>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// simplified css from App.css
+function Header () {
+    return(
+        <h1>Employee Details!!</h1>
+    )
+};
+
+export default Header;
